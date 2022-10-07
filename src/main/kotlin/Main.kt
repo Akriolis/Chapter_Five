520f55d1d321fb2a2ec55a7c0385c528d9b85be8
Akriolis <Akriolis@gmail.com>
2022-10-07 13:28:20 +0400

    Added few lines of practice code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -89,6 +89,61 @@ fun Person.isAdult() = age >= 21
  * Generated sequence
  */
 
+/**
+ * SAM constructors
+ */
+
+fun createAllDoneRunnable(): Runnable{
+    return Runnable { println ("All done!")}
+}
+
+/**
+ * Lambdas with receivers
+ *
+ * "with" and "apply"
+ */
+
+fun alphabet(): String{
+    val result = StringBuilder()
+    for (letter in 'A'..'Z'){
+        result.append(letter)
+    }
+    result.append("\nNow i know the alphabet!")
+    return result.toString()
+}
+
+fun alphabet2(): String{
+    val stringBuilder = StringBuilder()
+    return with (stringBuilder){
+        for (letter in 'A'..'Z'){
+            this.append(letter)
+        }
+        append("\nNow i know the alphabet!")
+        this.toString()
+    }
+}
+
+fun alphabet3() = with (StringBuilder()){
+    for (letter in 'A'..'Z'){
+        append(letter)
+    }
+    append("\nNow I know the alphabet!")
+    toString()
+}
+
+fun alphabet4() = StringBuilder().apply {
+    for (letter in 'A'..'Z'){
+        append(letter)
+    }
+    append("\nNow I know the alphabet! ")
+}.toString()
+
+fun alphabet5() = buildString{
+    for (letter in 'A'..'Z'){
+        append(letter)
+    }
+    append("\nNow I know the alphabet! ")
+}
 
 
 fun main(){
@@ -252,9 +307,19 @@ fun main(){
     val file = File ("/.HiddenDir/a.txt")
     println(file.isInsideHiddenDirectory())
 
-    
+    createAllDoneRunnable().run()
 
+    // with
+    println(alphabet())
 
+    println(alphabet2())
 
+    println(alphabet3())
+
+    // apply
+
+    println(alphabet4())
+
+    println(alphabet5())
 
 }
--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,6 +1,6 @@
 import kotlin.math.max
 
-data class Person (val name: String, val age: Int) {
+data class Person (val name: String, var age: Int) {
 
 
 }
